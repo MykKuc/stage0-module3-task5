2c82944bf72eb73e0bc40ec4329539dd735ac213
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-07-16 16:57:45 +0300

    All test passed, tasks completed.

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+
+       int  first = 1;
+       int second = 10;
+       int  third = 100;
+        System.out.println(first + "" +  second + "" +  third);
+
+
+        double division = 11%2;
+        System.out.println(division);
+
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public void convert(float minutes) {
+    public static void convert(float minutes) {
+
+        double seconds = minutes * 60;
+        System.out.println(seconds);
     }
+
+
 }
